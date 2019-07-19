ad87e7d5b7e250ee9c022fd475fa25cd263ae5ac
lei0904 <964358140@qq.com>
2019-07-19 16:09:04 +0800

    日常检查

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 
-// const Nav = r => require.ensure([], () => r(require('@/views/nav/nav.vue')), 'Nav')
-// const MigrantsItem = r => require.ensure([], () => r(require('@/views/migrants/item')), 'MigrantsItem')
-// const MigrantsInfo = r => require.ensure([], () => r(require('@/views/migrants/info')), 'MigrantsInfo')
-// const MigrantsAudit = r => require.ensure([], () => r(require('@/views/migrants/audit')), 'MigrantsAudit')
-
-/*
-const Migrants = r => require.ensure([], () => r(require('@/views/migrants/index.vue')), 'Migrants')
-const MigrantsMinePage = r => require.ensure([], () => r(require('@/views/migrants/migrants_mine_page.vue')), 'MigrantsMinePage')
-const MigrantsAuditPage = r => require.ensure([], () => r(require('@/views/migrants/migrants_audit_page.vue')), 'MigrantsAuditPage')
-const MigrantsDonePage = r => require.ensure([], () => r(require('@/views/migrants/migrants_done_page.vue')), 'MigrantsDonePage')
-*/
-
-const Cause = r => require.ensure([], () => r(require('@/views/cause/cause')), 'Cause')
-const Area = r => require.ensure([], () => r(require('@/views/area/area')), 'Area')
-const Person = r => require.ensure([], () => r(require('@/views/person/person')), 'Person')
-const FlowNode = r => require.ensure([], () => r(require('@/views/flow_node/flow_node')), 'FlowNode')
-
-
-const Migrants = r => require.ensure([], () => r(require('@/views/migrants/page')), 'Migrants')
-const MigrantsMinePage = r => require.ensure([], () => r(require('@/views/migrants/page_mine')), 'ExternalTrafficMine')
-const MigrantsAuditPage = r => require.ensure([], () => r(require('@/views/migrants/page_audit')), 'ExternalTrafficAudit')
-const MigrantsDonePage = r => require.ensure([], () => r(require('@/views/migrants/page_done')), 'ExternalTrafficDone')
-
-
-const CriminalQuery = r => require.ensure([], () => r(require('@/views/criminalQuery/criminalQuery')), 'CriminalQuery')
-
-const Prison = r => require.ensure([], () => r(require('@/views/prison/prison')), 'Prison')
-const PrisonDetail = r => require.ensure([], () => r(require('@/views/prisonDetail/prisonDetail')), 'PrisonDetail')
-
-
 /*广西demo*/
 const Workbench = r => require.ensure([], () => r(require('@/views/workbench/workbench')), 'Workbench')
 const Bussiness = r => require.ensure([], () => r(require('@/views/bussiness/bussiness')), 'Bussiness')
@@ -302,33 +272,6 @@ var routes = [
     meta:{
       value:4
     }
-  },
-
-
-
-
-
-
-
-  {
-    path: '/migrants',
-    redirect: 'migrants_mine_page',
-    name: 'Migrants',
-    component: Migrants,
-    children: [
-      {
-        path: '/migrants_mine_page',
-        component: MigrantsMinePage
-      },
-      {
-        path: '/migrants_audit_page',
-        component: MigrantsAuditPage
-      },
-      {
-        path: '/migrants_done_page',
-        component: MigrantsDonePage
-      }
-    ]
   },
   {
     path: '/bluetoothsynch',
